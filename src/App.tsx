8dae239bccb22a3db8d6fe41a1e5d5c2dac8ef2b
Aditya Pise <101938972+adipise5@users.noreply.github.com>
2025-03-15 10:45:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -290,7 +290,7 @@ function App() {
               >
                 <div className="h-48 overflow-hidden">
                   <img 
-                    src="https://unsplash.com/photos/human-brain-toy-IHfOpAzzjHM" 
+                    src="https://images.unsplash.com/photo-1559757175-5700dde675bc?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                     alt="Brain Tumor Detection" 
                     className="w-full h-full object-cover"
                   />
